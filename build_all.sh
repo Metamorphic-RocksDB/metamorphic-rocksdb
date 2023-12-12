8176d6f18e824d68a99256ba55af08be86b36d37
jaso <jaso@cs.wisc.edu>
2023-12-12 03:13:04 +0000

    use metarocks instead of rocksdb

--- build_all.sh
+++ build_all.sh
@@ -1,19 +1,11 @@
-git clone https://github.com/facebook/rocksdb.git
-cd rocksdb
-mkdir build
-cd build
-cmake ..
-make -j 8
-cd ../..
-
 git clone https://github.com/apache/arrow.git
-cd arrow
+cd arrow/cpp
 mkdir build
 cd build
 cmake .. -DARROW_PARQUET=ON
 make -j 8
 make install
-cd ../..
+cd ../../..
 
 git clone --recurse-submodules https://github.com/aws/aws-sdk-cpp
 cd aws-sdk-cpp/
@@ -22,6 +14,14 @@ cd sdk_build/
 cmake .. -DCMAKE_BUILD_TYPE=Debug -DCMAKE_PREFIX_PATH=/usr/local/ -DCMAKE_INSTALL_PREFIX=/usr/local/  -DBUILD_ONLY="sqs"
 make -j 8
 make install
+
+git clone git@github.com:GeoffreyXue/metarocks.git
+cd metarocks
+mkdir build
+cd build
+cmake ..
+make -j 8
+cd ../..
 
 cd ../..
 mkdir build
